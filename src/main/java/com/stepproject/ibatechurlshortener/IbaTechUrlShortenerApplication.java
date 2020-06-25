c513d2003c01915975a943facd156ca2ecb69b97
Rafig9501 <rafiqmammedzade@gmail.com>
2020-06-25 10:59:52 +0400

    initial commit

---
+++ src/main/java/com/stepproject/ibatechurlshortener/IbaTechUrlShortenerApplication.java
@@ -0,0 +1,13 @@
+package com.stepproject.ibatechurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbaTechUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbaTechUrlShortenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stepproject/ibatechurlshortener/IbatechUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepproject.ibatechurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbatechUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
