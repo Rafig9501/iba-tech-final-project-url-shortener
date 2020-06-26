75f7c5753aad321933aa970cd34a44abbec62e86
Rafig9501 <rafiqmammedzade@gmail.com>
2020-06-26 14:06:54 +0400

    User and Url repositories added. closes #4

---
+++ src/main/java/com/stepproject/ibatechurlshortener/repository/UrlRepository.java
@@ -0,0 +1,9 @@
+package com.stepproject.ibatechurlshortener.repository;
+
+import com.stepproject.ibatechurlshortener.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UrlRepository extends JpaRepository<User,Long> {
+}
