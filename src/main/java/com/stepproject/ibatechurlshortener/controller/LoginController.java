f9c72f05135329b64081911e3c2973450e0d0b1f
Rafig9501 <rafiqmammedzade@gmail.com>
2020-07-05 12:47:51 +0400

    user details take from login page. closes #27

--- src/main/java/com/stepproject/ibatechurlshortener/controller/LoginController.java
+++ src/main/java/com/stepproject/ibatechurlshortener/controller/LoginController.java
@@ -1,21 +1,30 @@
 package com.stepproject.ibatechurlshortener.controller;
 
+import com.stepproject.ibatechurlshortener.service.user.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
-    private final UserController userController;
+    private final UserService userService;
 
-    public LoginController(UserController userController) {
-        this.userController = userController;
+    public LoginController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping("/login")
     public String login(){
-        System.out.println("@GetMapping public String login()");
         return "html/login";
+    }
+
+    @GetMapping("/landing")
+    public String getStarted(Authentication authentication){
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        userService.findByEmail(user.getUsername());
+        System.out.println(user.getUsername());
+        return "html/landing";
     }
 }
